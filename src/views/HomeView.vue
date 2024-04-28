de586d41842224b5403bc93fe4c4031dd8ea8eea
qyzhg <qyzhg@qyzhg.com>
2024-04-28 13:43:35 +0800

    feat: 更新错误结构

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -204,72 +204,85 @@ const getHistory = async () => {
     <main>
       <TheItem :receivedMessage="message" :receivedMessageList="history" />
     </main>
-    <div v-if="httpIsError">
-      <h3>HTTP错误</h3>
+
+    <el-divider border-style="dashed">
+      <span>数据统计</span>
+    </el-divider>
+
+    <el-space direction="vertical" :size="35">
+      <div v-if="httpIsError">
+        <h3>HTTP错误</h3>
+        <el-table
+            :data="httpData"
+            style="width: 90rem"
+            :stripe="true">
+          <el-table-column prop="name" label="名称" show-overflow-tooltip width="140px" />
+          <el-table-column prop="code" label="状态码" show-overflow-tooltip width="80px" :formatter="row => row.code === 0? '-': row.code"/>
+          <el-table-column prop="message" label="错误信息" show-overflow-tooltip />
+          <el-table-column prop="source" label="来源" show-overflow-tooltip width="280px"/>
+          <el-table-column prop="url" label="url" show-overflow-tooltip width="300px"/>
+          <el-table-column prop="count" label="次数" width="120px"/>
+        </el-table>
+      </div>
+      <div v-if="assertIsError">
+        <h3>断言错误</h3>
+        <el-table
+          :data="assertData"
+          style="width: 90rem; margin-top: 1rem"
+          :stripe="true"
+        >
+          <el-table-column prop="name" label="名称" show-overflow-tooltip width="140px" />
+          <el-table-column prop="message" label="错误信息" show-overflow-tooltip />
+          <el-table-column prop="url" label="url" show-overflow-tooltip width="300px"/>
+          <el-table-column prop="count" label="次数" width="120px"/>
+        </el-table>
+      </div>
+      <div>
+      <h3>接口详情</h3>
       <el-table
-          :data="httpData"
-          style="width: 90rem"
-          :stripe="true">
-        <el-table-column prop="code" label="code" show-overflow-tooltip width="80px" />
-        <el-table-column prop="message" label="message" show-overflow-tooltip />
-        <el-table-column prop="count" label="count" width="120px"/>
-        <el-table-column prop="url" label="url" show-overflow-tooltip width="300px"/>
-      </el-table>
-    </div>
-    <div v-if="assertIsError">
-      <h3>断言错误</h3>
-      <el-table
-        :data="assertData"
-        style="width: 90rem; margin-top: 1rem"
-        :stripe="true"
-      >
-        <el-table-column prop="message" label="message" width="180" />
-        <el-table-column prop="count" label="count" width="180" />
-        <el-table-column prop="url" label="url" />
-      </el-table>
-    </div>
-    <h3>接口详情</h3>
-    <el-table
-      :data="api_resultsData"
-      style="width: 90rem; margin-top: 1rem"
-      :stripe="true"
-    >
-      <el-table-column prop="name" label="名称" show-overflow-tooltip width="140px"/>
-      <el-table-column prop="url" label="url" show-overflow-tooltip width="300px"/>
-      <el-table-column prop="method" label="请求方法" />
-      <el-table-column prop="rps" label="rps" :formatter="formatPrice" />
-      <el-table-column prop="total_requests" label="总请求数" />
-      <el-table-column prop="err_count" label="错误数量" />
-      <el-table-column prop="error_rate" label="错误率">
-        <template v-slot:default="scope">
+            :data="api_resultsData"
+            style="width: 90rem; margin-top: 1rem"
+            :stripe="true"
+        >
+          <el-table-column prop="name" label="名称" show-overflow-tooltip width="140px"/>
+          <el-table-column prop="url" label="url" show-overflow-tooltip width="300px"/>
+          <el-table-column prop="method" label="请求方法" />
+          <el-table-column prop="rps" label="rps" :formatter="formatPrice" />
+          <el-table-column prop="total_requests" label="总请求数" />
+          <el-table-column prop="err_count" label="错误数量" />
+          <el-table-column prop="error_rate" label="错误率">
+            <template v-slot:default="scope">
           <span :style="{ color: getColor(scope.row.error_rate) }">
             {{
               formatPriceWithPercentage(
-                scope.row,
-                "error_rate",
-                scope.row.error_rate,
-                scope.$index
+                  scope.row,
+                  "error_rate",
+                  scope.row.error_rate,
+                  scope.$index
               )
             }}
           </span>
-        </template>
-      </el-table-column>
-      <el-table-column prop="min_response_time" label="最小响应时间(ms)" />
-      <el-table-column prop="max_response_time" label="最大响应时间(ms)" />
-      <el-table-column prop="median_response_time" label="中位响应时间(ms)" />
-      <el-table-column prop="response_time_95" label="95位响应时间(ms)" />
-      <el-table-column prop="response_time_99" label="99位响应时间(ms)" />
-      <el-table-column
-        prop="throughput_per_second_kb"
-        label="每秒响应数据（kb）"
-        :formatter="formatPrice"
-      />
-      <el-table-column
-        prop="total_data_kb"
-        label="总响应数据量（kb）"
-        :formatter="formatPrice"
-      />
-    </el-table>
+            </template>
+          </el-table-column>
+          <el-table-column prop="min_response_time" label="最小响应时间(ms)" />
+          <el-table-column prop="max_response_time" label="最大响应时间(ms)" />
+          <el-table-column prop="median_response_time" label="中位响应时间(ms)" />
+          <el-table-column prop="response_time_95" label="95位响应时间(ms)" />
+          <el-table-column prop="response_time_99" label="99位响应时间(ms)" />
+          <el-table-column
+              prop="throughput_per_second_kb"
+              label="每秒响应数据（kb）"
+              :formatter="formatPrice"
+          />
+          <el-table-column
+              prop="total_data_kb"
+              label="总响应数据量（kb）"
+              :formatter="formatPrice"
+          />
+        </el-table>
+      </div>
+    </el-space>
+
     <footer>
       powered by
       <a
